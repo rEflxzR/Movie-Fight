5f6610322e250ae147eee583ee112cccd2d85de0
rEflxzR <anshu.singh1429@gmail.com>
2020-08-11 20:54:04 +0530

    Just Before Rendering Movie Details

--- index.js
+++ index.js
@@ -16,61 +16,43 @@ const fetchData = async (searchterm) => {
 
 // INPUT BOXs
 
-const root = document.querySelector('.autocomplete')
-
-root.innerHTML = `
-    <label><strong>Search for a Movie</strong></label>
-    <input class="input" />
-    <div class="dropdown">
-        <div class="dropdown-menu">
-            <div class="dropdown-content results"></div>
-        </div>
-    </div>
-`
-
-const input = document.querySelector('input')
-const dropdown = document.querySelector('.dropdown')
-const result = document.querySelector('.results')
-
-
-const onInput = async (event) => {
-    const movies = await fetchData(event.target.value)
-
-    if(!movies.length) {
-        dropdown.classList.remove('is-active')
-        return
-    }
-    
-    result.innerHTML = ""
-    dropdown.classList.add('is-active') 
-    const dropdownmenu = document.querySelector('.dropdown-menu')
-    dropdownmenu.style.width = '400px'
-
-    for(let movie of movies) {
-        const option = document.createElement('a')
+const autoCompleteConfig = {
+    renderOption(movie) {
         const imgsrc = movie.Poster === "N/A" ? "" : movie.Poster
-
-        option.classList.add('dropdown-item')
-        option.innerHTML = `
-            <img src="${imgsrc}" />
-            ${movie.Title}
-        `
-        option.addEventListener('click', () => {
-            dropdown.classList.remove('is-active')
-            input.value = movie.Title
-            onMovieSelect(movie)
+        return `<img src="${imgsrc}" />
+        ${movie.Title} (${movie.Year})`
+    },
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie)
+    },
+    inputValue(movie) {
+        return movie.Title
+    },
+    async fetchData(searchterm) {
+        const response = await axios.get('http://www.omdbapi.com/', {
+            params: {
+                apikey: 'c17b9d2b',
+                s: searchterm
+            }
         })
-
-        result.appendChild(option)
+    
+        if(response.data.Error) {
+            return []
+        }
+    
+        return response.data.Search
     }
 }
 
-input.addEventListener('input', debounce(onInput, 1000))
 
-document.addEventListener('click', (event) => {
-    if(!root.contains(event.target)) {
-        dropdown.classList.remove('is-active')
-    }
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('.left-autocomplete')
+})
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('.right-autocomplete')
 })
 
 
